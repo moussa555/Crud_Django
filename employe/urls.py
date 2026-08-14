9e80d613bd31ee7ccc8565ece6ba1f888688539f
moussamoussa <moussahadj01@gmail.com>
2026-08-14 03:26:17 -0400

    ajouter la fonctionnaliter ajouter et modifer employe

--- employe/urls.py
+++ employe/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import  views
 urlpatterns = [
     path('', views.liste_employes, name='liste_employes'),
+    path('ajouter', views.ajouter_employes, name='ajouter_employe'),
+path('modifier/<int:id>/', views.modifier_employes, name='modifier_employe'),
 ]
--- employe/views.py
+++ employe/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from employe.models import Employe
-
+from .forms import EmployeForm
 
 # Create your views here.
 
 def liste_employes(request):
-
     employes = Employe.objects.all()
     return render(request, 'employe/list.html', {'employes' : employes})
+
+def ajouter_employes (request):
+    form = EmployeForm(request.POST or None)
+    if form.is_valid() :
+        form.save()
+        return redirect('liste_employes')
+    return render(request, 'employe/formulaire.html', {'form': form})
+
+def modifier_employes (request, id):
+    employe = get_object_or_404(Employe, id=id)
+    form = EmployeForm(request.POST or None, instance=employe)
+    if form.is_valid() :
+        form.save()
+        return redirect('liste_employes')
+    return render(request, 'employe/formulaire.html', {'form': form})
+
