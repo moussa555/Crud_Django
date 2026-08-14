ba6a0f1af50f5ae6818d4863104b69574fbbe646
moussamoussa <moussahadj01@gmail.com>
2026-08-14 04:34:34 -0400

    add delete options

--- employe/urls.py
+++ employe/urls.py
@@ -3,5 +3,7 @@ from . import  views
 urlpatterns = [
     path('', views.liste_employes, name='liste_employes'),
     path('ajouter', views.ajouter_employes, name='ajouter_employe'),
-path('modifier/<int:id>/', views.modifier_employes, name='modifier_employe'),
+    path('modifier/<int:id>/', views.modifier_employes, name='modifier_employe'),
+    path('supprimer/<int:id>/', views.supprimer_employe, name='supprimer_employe'),
+
 ]
--- employe/views.py
+++ employe/views.py
@@ -23,3 +23,11 @@ def modifier_employes (request, id):
         return redirect('liste_employes')
     return render(request, 'employe/formulaire.html', {'form': form})
 
+def supprimer_employe (request, id):
+    employe = get_object_or_404(Employe, id=id)
+    if request.method == "POST" :
+        employe.delete()
+        return redirect('liste_employes')
+    return render(request, 'employe/confirmer_supprission.html', {'employe': employe})
+
+
