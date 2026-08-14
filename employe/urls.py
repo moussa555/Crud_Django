127edd92b108497d721e109dfdf9aa5fcca58569
moussamoussa <moussahadj01@gmail.com>
2026-08-13 23:40:16 -0400

    Initial commit

---
+++ employe/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import  views
+urlpatterns = [
+    path('', views.liste_employes, name='liste_employes'),
+]
---
+++ employe/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from employe.models import Employe
+
+
+# Create your views here.
+
+def liste_employes(request):
+
+    employes = Employe.objects.all()
+    return render(request, 'employe/list.html', {'employes' : employes})
